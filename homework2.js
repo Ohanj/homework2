1c5663d4c34208c66062d0ea85d9c9cd49887961
Ohanj <42995897+Ohanj@users.noreply.github.com>
2018-09-12 16:56:57 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,20 @@
+alert("my name jeff") // alert of jeff
+const passport_number = "";
+const name = "Jeff";
+const age = 30;
+
+const yearsLeft = 85 - age;
+const sicknessAge = 60 - age;
+
+if(yearsLeft >= 60){
+ console.log("Welcome to life");
+}else if(yearsLeft <= 10){
+  console.log("The end is near")
+}else {
+  	console.log("chill bro");
+  }
+
+if(sicknessAge <= 20) {
+  console.log("almost at deaths door");
+  }else {console.log("still got fuel in the tank")
+}
